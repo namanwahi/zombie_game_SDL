c6c1e67950a7df49cc3ffd6cc17fc698f13a31ed
nw316 <nw316@ic.ac.uk>
2017-10-04 12:00:24 +0100

    removed tabs

--- main.cpp
+++ main.cpp
@@ -73,65 +73,65 @@ int main(int argc, char * args[]) {
 
       resourceBar r_bar(SCREEN_WIDTH, SCREEN_HEIGHT);
 
-			int player_frame_index = 0;
-			int zombie_frame_index = 0;
-			int frame_iteration_counter = 0;
+      int player_frame_index = 0;
+      int zombie_frame_index = 0;
+      int frame_iteration_counter = 0;
 
-			//boolean to determine whether the endscreen should be shown
-			bool has_ended = false;
+      //boolean to determine whether the endscreen should be shown
+      bool has_ended = false;
 
-			//While game has not quit
+      //While game has not quit
       while (!quit) {
 
         //if player is on the endscreen
-				if (has_ended) {
-					while (SDL_PollEvent(&sdl_event)) {
-						if (sdl_event.type == SDL_QUIT) {
-								quit = true;
-						} else if (sdl_event.type == SDL_KEYDOWN) {
-							switch(sdl_event.key.keysym.sym) {
-						    case SDLK_r:
+        if (has_ended) {
+          while (SDL_PollEvent(&sdl_event)) {
+            if (sdl_event.type == SDL_QUIT) {
+                quit = true;
+            } else if (sdl_event.type == SDL_KEYDOWN) {
+              switch(sdl_event.key.keysym.sym) {
+                case SDLK_r:
                   //restart the game if r is pressed
-									has_ended = false;
+                  has_ended = false;
                   resetGame();
-									break;
-							}
-						}						
-					}
+                  break;
+              }
+            }           
+          }
           showEndgameScreen();
 
-				} else {
-					while (SDL_PollEvent(&sdl_event)) {
-						if (sdl_event.type == SDL_QUIT) {
-							quit = true;
-						}
-						g_player.handleEvent(sdl_event);
+        } else {
+          while (SDL_PollEvent(&sdl_event)) {
+            if (sdl_event.type == SDL_QUIT) {
+              quit = true;
+            }
+            g_player.handleEvent(sdl_event);
 
-					}
+          }
         
-					//spawn enemies
-					randomSpawnEnemy();
+          //spawn enemies
+          randomSpawnEnemy();
 
-					//Move the player and all enemies
-					g_player.move(SCREEN_WIDTH, SCREEN_HEIGHT);
+          //Move the player and all enemies
+          g_player.move(SCREEN_WIDTH, SCREEN_HEIGHT);
           auto it = enemies.begin();
           while (it != enemies.end()) {
-						(*it)->move(g_player, enemies);
-						it++;
+            (*it)->move(g_player, enemies);
+            it++;
           }
 
-					g_player.adjustAimAccuracy();
+          g_player.adjustAimAccuracy();
 
-					g_background_texture.render(0, 0, g_renderer.get());
+          g_background_texture.render(0, 0, g_renderer.get());
 
-			   	//handle all shots 
+          //handle all shots 
           it = enemies.begin();
           while (it != enemies.end()) {
               handleShot(g_player, **it);
               if ((*it)->isDead()) {
                   it = enemies.erase(it);
-									g_score++;
-							} else {
+                  g_score++;
+              } else {
                   it++;
               }
           }
@@ -139,53 +139,53 @@ int main(int argc, char * args[]) {
           enemyAttackHandler();
 
           if (frame_iteration_counter++ % FRAME_CHANGE_ITERATIONS == FRAME_CHANGE_ITERATIONS - 1) {
-						frame_iteration_counter = 0;
-					
-						//if the player is moving forward advance the frame
-						if (g_player.getMovingState() == Player::MOVING_FORWARD) {
-							//if the next frame is ready to be shown
-							player_frame_index = (player_frame_index + 1)%PLAYER_FRAMES_COUNT;
-						} else if (g_player.getMovingState() == Player::MOVING_BACKWARD) {
-							//if the previous frame is ready to be shown
-							player_frame_index--;
-							
-							//if index is negative then circle around
-							if (player_frame_index < 0) {
-								player_frame_index += PLAYER_FRAMES_COUNT;
-							}
-						}
-						zombie_frame_index = (zombie_frame_index + 1)%ENEMY_FRAMES_COUNT;
+            frame_iteration_counter = 0;
+          
+            //if the player is moving forward advance the frame
+            if (g_player.getMovingState() == Player::MOVING_FORWARD) {
+              //if the next frame is ready to be shown
+              player_frame_index = (player_frame_index + 1)%PLAYER_FRAMES_COUNT;
+            } else if (g_player.getMovingState() == Player::MOVING_BACKWARD) {
+              //if the previous frame is ready to be shown
+              player_frame_index--;
+              
+              //if index is negative then circle around
+              if (player_frame_index < 0) {
+                player_frame_index += PLAYER_FRAMES_COUNT;
+              }
+            }
+            zombie_frame_index = (zombie_frame_index + 1)%ENEMY_FRAMES_COUNT;
           }
 
-				  //check is the player has died
-					if (g_player.isDead()) {
-						has_ended = true;
-					}
+          //check is the player has died
+          if (g_player.isDead()) {
+            has_ended = true;
+          }
 
           //render player and all enemies
           g_player.render(g_renderer.get(), g_player_texture[player_frame_index]);
           it = enemies.begin();
           while (it != enemies.end()) {
-						if (!(*it)->is_super) {
-							(*it)->render(g_renderer.get(), g_enemy_texture[zombie_frame_index]);
-						} else {
-							(*it)->render(g_renderer.get(), g_super_enemy_texture[zombie_frame_index]);
-						}
-						it++;
+            if (!(*it)->is_super) {
+              (*it)->render(g_renderer.get(), g_enemy_texture[zombie_frame_index]);
+            } else {
+              (*it)->render(g_renderer.get(), g_super_enemy_texture[zombie_frame_index]);
+            }
+            it++;
           }
 
-					//render score
+          //render score
           renderScore(SCREEN_WIDTH, 0);
 
           //render resource bar
           r_bar.render(g_player, g_renderer.get());
-					
+          
           //update screen
           SDL_RenderPresent(g_renderer.get());
-				}
-			}
-		}
-	}
+        }
+      }
+    }
+  }
   //close the game
   close();
 
@@ -222,10 +222,10 @@ bool initialiseGame() {
           std::cerr << IMG_GetError();
           return false;
         }
-				if (TTF_Init() == -1) {
-					std::cerr << TTF_GetError();
-					return false;
-				}
+        if (TTF_Init() == -1) {
+          std::cerr << TTF_GetError();
+          return false;
+        }
       }
     }
   }
@@ -243,87 +243,87 @@ bool loadMedia() {
     success = false;
   }
 
-	
+  
   if (!g_player_texture[1].loadFromFile("./pixel_art/separate_pics/sprite_soldier1.png", g_renderer.get(), Player::WIDTH)) {
     std::cerr << "Failed to load player texture 1\n";
     success = false;
   }
 
-	
+  
   if (!g_player_texture[2].loadFromFile("./pixel_art/separate_pics/sprite_soldier2.png", g_renderer.get(), Player::WIDTH)) {
     std::cerr << "Failed to load player texture 2\n";
     success = false;
   }
 
-	
+  
   if (!g_player_texture[3].loadFromFile("./pixel_art/separate_pics/sprite_soldier3.png", g_renderer.get(), Player::WIDTH)) {
     std::cerr << "Failed to load player texture 3\n";
     success = false;
   }
 
-	//load nnemy textures
-	if (!g_enemy_texture[0].loadFromFile("./pixel_art/separate_pics/zombie_sprite0.png", g_renderer.get(), Enemy::ENEMY_WIDTH)) {
+  //load nnemy textures
+  if (!g_enemy_texture[0].loadFromFile("./pixel_art/separate_pics/zombie_sprite0.png", g_renderer.get(), Enemy::ENEMY_WIDTH)) {
     std::cerr << "Failed to load Enemy texture 0\n";
-    success = false;		
-	}
+    success = false;    
+  }
 
-	
-	if (!g_enemy_texture[1].loadFromFile("./pixel_art/separate_pics/zombie_sprite1.png", g_renderer.get(), Enemy::ENEMY_WIDTH)) {
+  
+  if (!g_enemy_texture[1].loadFromFile("./pixel_art/separate_pics/zombie_sprite1.png", g_renderer.get(), Enemy::ENEMY_WIDTH)) {
     std::cerr << "Failed to load Enemy texture 1\n";
     success = false;
-	}
-	
-	if (!g_enemy_texture[2].loadFromFile("./pixel_art/separate_pics/zombie_sprite2.png", g_renderer.get(), Enemy::ENEMY_WIDTH)) {
+  }
+  
+  if (!g_enemy_texture[2].loadFromFile("./pixel_art/separate_pics/zombie_sprite2.png", g_renderer.get(), Enemy::ENEMY_WIDTH)) {
     std::cerr << "Failed to load Enemy texture 2\n";
-    success = false;		
-	}
-	
-	if (!g_enemy_texture[3].loadFromFile("./pixel_art/separate_pics/zombie_sprite3.png", g_renderer.get(), Enemy::ENEMY_WIDTH)) {
+    success = false;    
+  }
+  
+  if (!g_enemy_texture[3].loadFromFile("./pixel_art/separate_pics/zombie_sprite3.png", g_renderer.get(), Enemy::ENEMY_WIDTH)) {
     std::cerr << "Failed to load Enemy texture 3\n";
-    success = false;		
-	}
+    success = false;    
+  }
 
   //load super Enemy
   if (!g_super_enemy_texture[0].loadFromFile("./pixel_art/separate_pics/super_zombie_sprite_0.png", g_renderer.get(), Enemy::ENEMY_WIDTH)) {
     std::cerr << "Failed to load super Enemy texture 0\n";
-    success = false;		
-	}
+    success = false;    
+  }
 
-	
-	if (!g_super_enemy_texture[1].loadFromFile("./pixel_art/separate_pics/super_zombie_sprite_1.png", g_renderer.get(), Enemy::ENEMY_WIDTH)) {
+  
+  if (!g_super_enemy_texture[1].loadFromFile("./pixel_art/separate_pics/super_zombie_sprite_1.png", g_renderer.get(), Enemy::ENEMY_WIDTH)) {
     std::cerr << "Failed to load super Enemy texture 1\n";
     success = false;
-	}
-	
-	if (!g_super_enemy_texture[2].loadFromFile("./pixel_art/separate_pics/super_zombie_sprite_2.png", g_renderer.get(), Enemy::ENEMY_WIDTH)) {
+  }
+  
+  if (!g_super_enemy_texture[2].loadFromFile("./pixel_art/separate_pics/super_zombie_sprite_2.png", g_renderer.get(), Enemy::ENEMY_WIDTH)) {
     std::cerr << "Failed to load super Enemy texture 2\n";
-    success = false;		
-	}
-	
-	if (!g_super_enemy_texture[3].loadFromFile("./pixel_art/separate_pics/super_zombie_sprite_3.png", g_renderer.get(), Enemy::ENEMY_WIDTH)) {
+    success = false;    
+  }
+  
+  if (!g_super_enemy_texture[3].loadFromFile("./pixel_art/separate_pics/super_zombie_sprite_3.png", g_renderer.get(), Enemy::ENEMY_WIDTH)) {
     std::cerr << "Failed to load super Enemy texture 3\n";
-    success = false;		
-	}
+    success = false;    
+  }
 
-	font_ptr font(TTF_OpenFont("fonts/comic.ttf", 30), &TTF_CloseFont);
+  font_ptr font(TTF_OpenFont("fonts/comic.ttf", 30), &TTF_CloseFont);
   g_font = std::move(font);
-	if (!g_font) {
-		std::cerr << TTF_GetError();
-		success = false;
-	}
+  if (!g_font) {
+    std::cerr << TTF_GetError();
+    success = false;
+  }
 
-	if (!g_background_texture.loadFromFile("./pixel_art/grass_background.png", g_renderer.get(), SCREEN_WIDTH)) {
-		std::cerr << SDL_GetError();
-		success = false;
-	}
-		
+  if (!g_background_texture.loadFromFile("./pixel_art/grass_background.png", g_renderer.get(), SCREEN_WIDTH)) {
+    std::cerr << SDL_GetError();
+    success = false;
+  }
+    
   return success;
 }
 
 void close() {
   IMG_Quit();
   SDL_Quit();
-	TTF_Quit();
+  TTF_Quit();
 }
 
 void randomSpawnEnemy() {
@@ -342,39 +342,39 @@ void randomSpawnEnemy() {
 }
 
 void enemyAttackHandler() {
-	int reg_dmg = 1;
-	int spc_dmg = 3;
+  int reg_dmg = 1;
+  int spc_dmg = 3;
   auto it = enemies.begin();
-	while (it != enemies.end()) {
-		if (findDistance(g_player.getVisibleCentre(), (*it)->getCentre()) < Enemy::ENEMY_WIDTH * 0.75) {
-			//choose what damage depending on the type of enemy attacking the player
+  while (it != enemies.end()) {
+    if (findDistance(g_player.getVisibleCentre(), (*it)->getCentre()) < Enemy::ENEMY_WIDTH * 0.75) {
+      //choose what damage depending on the type of enemy attacking the player
       g_player.takeDamage((**it).is_super ? spc_dmg : reg_dmg);
     }
     it++;
-	}
+  }
 }
 
 void renderScore(int x_pos, int y_pos) {
-	TextureWrapper scoreTexture;
-	std::string number = std::to_string(g_score);
-	std::string score_str = "Score: ";
-	SDL_Color color = {0,0,0};
-	if (!scoreTexture.loadTTFTexture(score_str + number, g_font.get(), color, g_renderer.get())) {
-		std::cerr << "error loading score texture\n";
-	}
+  TextureWrapper scoreTexture;
+  std::string number = std::to_string(g_score);
+  std::string score_str = "Score: ";
+  SDL_Color color = {0,0,0};
+  if (!scoreTexture.loadTTFTexture(score_str + number, g_font.get(), color, g_renderer.get())) {
+    std::cerr << "error loading score texture\n";
+  }
 
-	scoreTexture.render(x_pos - 2*scoreTexture.getWidth(), y_pos, g_renderer.get());
-	
+  scoreTexture.render(x_pos - 2*scoreTexture.getWidth(), y_pos, g_renderer.get());
+  
 }
 
 void showEndgameScreen() {
-	TextureWrapper endGameTexture;
-	std::string message = "Game Over! Press R to replay";
-	SDL_Color color = {0xff, 0x0, 0x0};
-	if (!endGameTexture.loadTTFTexture(message, g_font.get(), color, g_renderer.get())) {
-		std::cerr << "error loading end game texture";
-	}
-	endGameTexture.render(SCREEN_WIDTH/2 - endGameTexture.getWidth()/2, SCREEN_HEIGHT/2 - endGameTexture.getHeight()/2, g_renderer.get());
+  TextureWrapper endGameTexture;
+  std::string message = "Game Over! Press R to replay";
+  SDL_Color color = {0xff, 0x0, 0x0};
+  if (!endGameTexture.loadTTFTexture(message, g_font.get(), color, g_renderer.get())) {
+    std::cerr << "error loading end game texture";
+  }
+  endGameTexture.render(SCREEN_WIDTH/2 - endGameTexture.getWidth()/2, SCREEN_HEIGHT/2 - endGameTexture.getHeight()/2, g_renderer.get());
   SDL_RenderPresent(g_renderer.get());
 }
 
